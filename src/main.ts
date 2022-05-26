2e819d9983e71bfdf0cd56bdbd25693c7dc28c03
Vadim Korolik <vkorolik@gmail.com>
2022-05-26 10:28:56 -0700

    highlight: add session recording

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { H } from "highlight.run";
+
+// See https://app.highlight.run/setup for your project ID.
+H.init("YOUR_PROJECT_ID", {
+  debug: { clientInteractions: true, domRecording: true },
+  networkRecording: {
+    enabled: true,
+    recordHeadersAndBody: true,
+  },
+  tracingOrigins: true,
+  scriptUrl: "http://localhost:8080/dist/index.js",
+  backendUrl: "http://localhost:8082/public",
+});
 
 if (environment.production) {
   enableProdMode();
